5ecb2ec551f02faab3a2cafc9e9347c740985265
atramirez <adenramirez166@gmail.com>
2023-03-10 12:33:04 -0700

    actually test and fix, initial script

--- main.py
+++ main.py
@@ -10,34 +10,50 @@ import datetime
 import csv
 
 def ping_google():
-    """
-    returns: True if google can be reached
-    """
-    return subprocess.call(['ping', '-c', '1', 'google.com'])
+    return subprocess.check_output(['ping', '-c', '1', 'google.com'])
 
-def add_entry(time, internet_down):
-    with open("down_log.csv", "w") as log_file:
+def get_internet_up_status():
+    try:
+        ping_google()
+        up_status = True 
+    except subprocess.CalledProcessError:
+        up_status = False
+    return up_status
+
+def add_entry(time, internet_down, file):
+    with open(file, "a") as log_file:
         writer = csv.writer(log_file)
         writer.writerow([time, internet_down])
+        log_file.close()
 
 def main():
     internet_down = False
-    with open("down_log.csv", "w") as log_file:
+    file_name = f"downlog-{datetime.datetime.now()}.csv"
+    with open(file_name, "w") as log_file:
         writer = csv.writer(log_file)
-        writer.writerow(['Timestamp', 'Status'])
-        
-    add_entry(datetime.datetime.now(), ping_google())
+        writer.writerow(['Timestamp', 'Status(Up=1)'])
+        log_file.close()
+
+    print("Startup: Initial Ping")
+    print("----------------------------------------------------")   
+    add_entry(datetime.datetime.now(), get_internet_up_status(), file_name)
 
     while(True):
         time.sleep(60)
+        print("doing it ")
         cur_time = datetime.datetime.now()
-        cur_status = ping_google() 
-        if cur_status != 0 and not internet_down: # If bad return and internet is currently up
-            add_entry(cur_time, True)
+        up_status = get_internet_up_status()
+        
+        if up_status == False and not internet_down: # If bad return and internet is currently up
+            add_entry(cur_time, False, file_name)
+            internet_down = True
             print(f"{cur_time}:** Updating Internet Status to Down **")
-        elif cur_status == 0 and internet_down: # If good return and internet is currently down
-            add_entry(cur_time, False)
+            print("----------------------------------------------------") 
+        elif up_status == True and internet_down: # If good return and internet is currently down
+            add_entry(cur_time, True, file_name)
+            internet_down = False
             print(f"{cur_time}:** Updating Internet Status to Up **")
+            print("----------------------------------------------------") 
 
 
 if __name__ == "__main__":
